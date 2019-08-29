462cc4fe9528705146b8b2d43d160d7ffaef300f
xuwenhao1995 <849011310@qq.com>
2019-08-29 22:46:29 +0800

    ss

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -9,15 +9,21 @@
     <!-- 搜索 -->
     <el-row class="search">
       <el-col>
-        <el-input placeholder="请输入内容" v-model="query" class="inputSearch">
+        <el-input
+          @clear="getUserList()"
+          clearable
+          placeholder="请输入内容"
+          v-model="query"
+          class="inputSearch"
+        >
           <el-button slot="append" icon="el-icon-search" @click="getUserList()"></el-button>
         </el-input>
-        <el-button type="success">添加用户</el-button>
+        <el-button plain @click="showDialog()" type="success">添加用户</el-button>
       </el-col>
     </el-row>
 
     <!-- 表格 -->
-    <el-table :data="userList" style="width: 100%">
+    <el-table height="550px" :data="userList" style="width: 100%">
       <el-table-column type="index" label width="40"></el-table-column>
       <el-table-column prop="username" label="姓名" width="80"></el-table-column>
       <el-table-column prop="email" label="邮箱"></el-table-column>
@@ -37,22 +43,58 @@
       </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="userList">
-          <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
+          <el-button
+            @click="showEditDialog(userList.row)"
+            size="mini"
+            plain
+            type="primary"
+            icon="el-icon-edit"
+            circle
+          ></el-button>
           <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
-          <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+          <el-button
+            @click="showDel(userList.row.id)"
+            size="mini"
+            plain
+            type="danger"
+            icon="el-icon-delete"
+            circle
+          ></el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <el-pagination
+      class="pagination"
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="pagenum"
       :page-sizes="[2, 4, 6, 8]"
-      :page-size="2"
+      :page-size="pagesize"
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     ></el-pagination>
+
+    <el-dialog :title="title" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="用户名" label-width="100px">
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" label-width="100px">
+          <el-input v-model="form.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" label-width="100px">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addOrEditUser()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -64,7 +106,16 @@ export default {
       total: -1,
       query: "",
       pagenum: 1,
-      pagesize: 2
+      pagesize: 6,
+      title: "",
+
+      dialogFormVisible: false,
+      form: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      }
     };
   },
   created() {
@@ -73,10 +124,63 @@ export default {
   methods: {
     //分页
     handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
+      this.pagesize = val;
+      this.pagenum = 1;
+      this.getUserList();
     },
     handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+      this.pagenum = val;
+      this.getUserList();
+    },
+    showDialog() {
+      this.title = "增加用户"
+      this.dialogFormVisible = true
+    },
+    showEditDialog(user) {
+      this.title = "修改用户"
+      this.form = user
+      this.dialogFormVisible = true
+    },
+    showDel(userId) {
+      this.$confirm("删除用户?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          const res = await this.$http.delete(`users/${userId}`);
+          const {
+            meta: { status, msg }
+          } = res.data;
+          if (status === 200) {
+            this.pagenum = 1;
+            this.getUserList();
+            this.$message.success(msg);
+          } else {
+            this.$message.warning(msg);
+          }
+        })
+        .catch(() => {
+          this.$message.info("已取消删除");
+        });
+    },
+
+    async addOrEditUser() {
+      if (this.title === "修改用户") {
+      } else {
+        const res = await this.$http.post("users", this.form);
+        const {
+          meta: { status, msg }
+        } = res.data;
+        if (status === 201) {
+          this.$message.success(msg);
+          this.getUserList();
+          this.form = {};
+        } else {
+          this.$message.warning(msg);
+        }
+        this.dialogFormVisible = false;
+      }
     },
 
     async getUserList() {
@@ -109,6 +213,11 @@ export default {
 
   .inputSearch {
     width: 300px;
+  }
+
+  .pagination {
+    position: fixed;
+    bottom: 30px;
   }
 }
 </style>
